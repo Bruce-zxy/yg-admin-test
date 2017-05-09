af30dba5dadfa1c33bcb02ab9700d1a314afd4b8
bruce-zxy <bruce_zxy@163.com>
2017-05-09 12:03:26 +0800

    initial

---
+++ src/components/introduce.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// introduce
+export default class myIntroduce extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div className="ani-box">
+                这仅仅是一个关于AntD与React的测试项目. <br/>
+                This is Only a test project about AntD and React technology.
+            </div>
+        )
+    }
+}
